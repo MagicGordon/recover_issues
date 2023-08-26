351c86dc62f563d524e7f725c02d15ba3fe6b2b4
MagicGordon <19465870+MagicGordon@users.noreply.github.com>
2023-08-26 17:48:14 +0800

    init

---
+++ index.mjs
@@ -0,0 +1,44 @@
+import * as nep141Xerc20 from '@near-eth/nep141-erc20'
+import * as ethXnear from '@near-eth/near-ether'
+import * as transfers from '@near-eth/client'
+import { ethers } from 'ethers'
+import { providers as najProviders } from 'near-api-js'
+
+async function main() {
+
+    transfers.setEthProvider(new ethers.providers.InfuraProvider(
+        process.env.ethNetworkId === 'main' ? 'mainnet' : process.env.ethNetworkId,
+        process.env.INFURA_ID
+    ))
+
+    transfers.setBridgeParams({
+        nearEventRelayerMargin: Number(process.env.nearEventRelayerMargin),
+        sendToNearSyncInterval: Number(process.env.sendToNearSyncInterval),
+        sendToEthereumSyncInterval: Number(process.env.sendToEthereumSyncInterval),
+        ethChainId: Number(process.env.ethChainId),
+        erc20Abi: process.env.ethErc20AbiText,
+        erc20LockerAddress: process.env.ethLockerAddress,
+        erc20LockerAbi: process.env.ethLockerAbiText,
+        nep141Factory: process.env.nearTokenFactoryAccount,
+        nearTokenFactoryAccount: process.env.nearTokenFactoryAccount,
+        nativeNEARLockerAddress: process.env.nativeNEARLockerAddress,
+        eNEARAddress: process.env.eNEARAddress,
+        eNEARAbi: process.env.eNEARAbiText,
+        etherCustodianAddress: process.env.etherCustodianAddress,
+        etherCustodianAbi: process.env.etherCustodianAbiText,
+        auroraEvmAccount: process.env.auroraEvmAccount,
+        etherExitToEthereumPrecompile: process.env.exitToEthereumPrecompile,
+        ethClientAddress: process.env.ethClientAddress,
+        ethClientAbi: process.env.ethNearOnEthClientAbiText,
+        nearClientAccount: process.env.nearClientAccount
+    })
+
+    // console.log(await ethXnear.naturalETH.recover('0x8a98332f70ae537a3d5d9a642b7b68af9033c5a1a4008e8eb5ac1b875daf085e', {
+    //     nearProvider: new najProviders.JsonRpcProvider({ url: process.env.nearNodeUrl })
+    // }))
+
+    console.log(await nep141Xerc20.naturalErc20.recover('0x87a48690b34d7e54c4dc4a2ca8bc8689193bed4c9ae0969d58a94f4af0e77788', {
+        nearProvider: new najProviders.JsonRpcProvider({ url: process.env.nearNodeUrl })
+    }))
+}
+main()
